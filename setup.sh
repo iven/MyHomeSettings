a5bdd645979dc31817678edf1cb815bafc4b6232
Iven Hsu <ivenvd@gmail.com>
2022-03-28 03:18:07 +0800

    添加 systemd 环境变量目录

--- setup.sh
+++ setup.sh
@@ -6,6 +6,7 @@ ln -s ${PWD}/.global_ignore ~/
 ln -s ${PWD}/.tmux.conf ~/
 ln -s ${PWD}/.tmuxline.conf ~/
 ln -s ${PWD}/.config/archey4 ~/.config/
+ln -s ${PWD}/.config/environment.d ~/.config/
 ln -s ${PWD}/.config/flake8 ~/.config/
 ln -s ${PWD}/.config/htop ~/.config/
 ln -s ${PWD}/.config/nvim ~/.config/
